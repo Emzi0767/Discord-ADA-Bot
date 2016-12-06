f109d5c302c2a3185cdcc3a564a13f374e905146
Emzi0767 <matiz100@windowslive.com>
2016-12-06 20:15:42 +0100

    Ported Stallman Plugin.

---
+++ Emzi0767.Ada.Plugin.Stallman/StallmanCommandModule.cs
@@ -0,0 +1,83 @@
+﻿using System.Threading.Tasks;
+using Discord;
+using Emzi0767.Ada.Attributes;
+using Emzi0767.Ada.Commands;
+using Emzi0767.Ada.Commands.Permissions;
+
+namespace Emzi0767.Ada.Plugin.Stallman
+{
+    public class StallmanCommandModule : IAdaCommandModule
+    {
+        public string Name { get { return "GNU/Stallman Commands"; } }
+
+        [AdaCommand("stallmanenable", "Enables the GNU/Stallman plugin for this guild. This command can only be used by guild administrators.", CheckerId = "CoreAdminChecker", CheckPermissions = true, RequiredPermission = AdaPermission.ManageGuild)]
+        public async Task EnableStallman(AdaCommandContext ctx)
+        {
+            var gld = ctx.Guild;
+
+            await ctx.Message.DeleteAsync();
+            StallmanPlugin.Instance.Enable(gld.Id);
+
+            var embed = this.PrepareEmbed("Success", "GNU/Stallman plugin was enabled for this guild.", EmbedType.Success);
+            await ctx.Channel.SendMessageAsync("", false, embed);
+        }
+
+        [AdaCommand("stallmandisable", "Disables the GNU/Stallman plugin for this guild. This command can only be used by guild administrators.", CheckerId = "CoreAdminChecker", CheckPermissions = true, RequiredPermission = AdaPermission.ManageGuild)]
+        public async Task DisableStallman(AdaCommandContext ctx)
+        {
+            var gld = ctx.Guild;
+
+            await ctx.Message.DeleteAsync();
+            StallmanPlugin.Instance.Disable(gld.Id);
+
+            var embed = this.PrepareEmbed("Success", "GNU/Stallman plugin was disabled for this guild.", EmbedType.Success);
+            await ctx.Channel.SendMessageAsync("", false, embed);
+        }
+
+        private EmbedBuilder PrepareEmbed(EmbedType type)
+        {
+            var embed = new EmbedBuilder();
+            switch (type)
+            {
+                case EmbedType.Info:
+                    embed.Color = new Color(0, 127, 255);
+                    break;
+
+                case EmbedType.Success:
+                    embed.Color = new Color(127, 255, 0);
+                    break;
+
+                case EmbedType.Warning:
+                    embed.Color = new Color(255, 255, 0);
+                    break;
+
+                case EmbedType.Error:
+                    embed.Color = new Color(255, 127, 0);
+                    break;
+
+                default:
+                    embed.Color = new Color(255, 255, 255);
+                    break;
+            }
+            embed.ThumbnailUrl = AdaBotCore.AdaClient.CurrentUser.AvatarUrl;
+            return embed;
+        }
+
+        private EmbedBuilder PrepareEmbed(string title, string desc, EmbedType type)
+        {
+            var embed = this.PrepareEmbed(type);
+            embed.Title = title;
+            embed.Description = desc;
+            return embed;
+        }
+
+        private enum EmbedType : uint
+        {
+            Unknown,
+            Success,
+            Error,
+            Warning,
+            Info
+        }
+    }
+}
---
+++ Emzi0767.Ada.Plugin.Stallman/StallmanPluginConfig.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using Emzi0767.Ada.Config;
+using Newtonsoft.Json.Linq;
+
+namespace Emzi0767.Ada.Plugin.Stallman
+{
+    public class StallmanPluginConfig : IAdaPluginConfig
+    {
+        public IAdaPluginConfig DefaultConfig { get { return new StallmanPluginConfig(); } }
+
+        internal List<ulong> DisabledGuilds { get; set; }
+
+        public StallmanPluginConfig()
+        {
+            this.DisabledGuilds = new List<ulong>();
+        }
+
+        public void Load(JObject jo)
+        {
+            var disabled = (JArray)jo["disabled"];
+            foreach (var xd in disabled)
+                this.DisabledGuilds.Add((ulong)xd);
+        }
+
+        public JObject Save()
+        {
+            var disabled = new JArray();
+            foreach (var xd in this.DisabledGuilds)
+                disabled.Add(xd);
+            var jo = new JObject();
+            jo.Add("disabled", disabled);
+            return jo;
+        }
+    }
+}
